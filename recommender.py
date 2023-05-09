081377cfbb98a83846e0338ff9bddc1746981745
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-05-09 21:48:41 +0500

    Update recommender.py to use UCB to update preferences

--- recommender.py
+++ recommender.py
@@ -1,7 +1,10 @@
+import math
+
 class Recommender:
-    def __init__(self):
+    def __init__(self, c=1):
         self.items = set()
         self.user_preferences = {}
+        self.c = c
 
     def add_item(self, item):
         self.items.add(item)
@@ -14,4 +17,16 @@ class Recommender:
         if user_id not in self.user_preferences:
             self.user_preferences[user_id] = {}
 
-        self.user_preferences[user_id][item] = reward
+        n = len(self.user_preferences[user_id])
+        if n == 0:
+            self.user_preferences[user_id][item] = reward
+        else:
+            total_reward = sum(self.user_preferences[user_id].values())
+            avg_reward = total_reward / n
+            delta_i = math.sqrt(2 * math.log(n) / n)
+            ucb = avg_reward + self.c * delta_i
+            if item in self.user_preferences[user_id]:
+                self.user_preferences[user_id][item] = ((n - 1) / n) * self.user_preferences[user_id][item] + (1 / n) * reward
+            else:
+                self.user_preferences[user_id][item] = reward
+            self.user_preferences[user_id][item] += ucb
