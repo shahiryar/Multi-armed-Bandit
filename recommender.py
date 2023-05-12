0858820c88514bc80335182d11b97584e3ca9c2e
Shahiryar Saleem <shahiryarsaleem@gmail.com>
2023-05-12 23:16:48 +0500

    updated identifiers for users' preferences

--- recommender.py
+++ recommender.py
@@ -3,35 +3,34 @@ import math
 class Recommender:
     def __init__(self, c=1):
         self.items = set()
-        self.user_preferences = {}
+        self.users_preferences = {}
         self.c = c
 
     def add_item(self, item):
         self.items.add(item)
-        self.user_preferences[item] = {}
 
     def update_preferences(self, user_id, item, reward):
         if item not in self.items:
             raise ValueError("Invalid item")
 
-        if user_id not in self.user_preferences:
-            self.user_preferences[user_id] = {}
+        if user_id not in self.users_preferences:
+            self.users_preferences[user_id] = {}
 
-        n = len(self.user_preferences[user_id])
+        n = len(self.users_preferences[user_id])
         if n == 0:
-            self.user_preferences[user_id][item] = reward
+            self.users_preferences[user_id][item] = reward
         else:
-            total_reward = sum(self.user_preferences[user_id].values())
+            total_reward = sum(self.users_preferences[user_id].values())
             avg_reward = total_reward / n
             delta_i = math.sqrt(2 * math.log(n) / n)
             ucb = avg_reward + self.c * delta_i
-            if item in self.user_preferences[user_id]:
-                self.user_preferences[user_id][item] = ((n - 1) / n) * self.user_preferences[user_id][item] + (1 / n) * reward
+            if item in self.users_preferences[user_id]:
+                self.users_preferences[user_id][item] = ((n - 1) / n) * self.users_preferences[user_id][item] + (1 / n) * reward
             else:
-                self.user_preferences[user_id][item] = reward
-            self.user_preferences[user_id][item] += ucb
+                self.users_preferences[user_id][item] = reward
+            self.users_preferences[user_id][item] += ucb
 
     def get_top_n_preferences(self, user_id, n):
-        if user_id not in self.user_preferences:
+        if user_id not in self.users_preferences:
             return []
-        return sorted(self.user_preferences[user_id].items(), key=lambda x: x[1], reverse=True)[:n]
+        return sorted(self.users_preferences[user_id].items(), key=lambda x: x[1], reverse=True)[:n]
